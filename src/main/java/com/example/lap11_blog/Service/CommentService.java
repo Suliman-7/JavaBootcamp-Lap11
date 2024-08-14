7d990c0dcc114b7052282ad2c92a84b0297fefb9
Suliman Alodaib <107861497+Suliman-7@users.noreply.github.com>
2024-08-14 14:35:43 +0300

    Update CommentService.java

--- src/main/java/com/example/lap11_blog/Service/CommentService.java
+++ src/main/java/com/example/lap11_blog/Service/CommentService.java
@@ -2,7 +2,11 @@ package com.example.lap11_blog.Service;
 
 import com.example.lap11_blog.Api.ApiException;
 import com.example.lap11_blog.Model.Comment;
+import com.example.lap11_blog.Model.Post;
+import com.example.lap11_blog.Model.User;
 import com.example.lap11_blog.Repository.CommentRepository;
+import com.example.lap11_blog.Repository.PostRepository;
+import com.example.lap11_blog.Repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -15,16 +19,45 @@ import java.util.List;
 public class CommentService {
 
     private final CommentRepository commentRepository;
+    private final UserRepository userRepository;
+    private final PostRepository postRepository;
+
 
     public List<Comment> getAllComment() {
         return commentRepository.findAll();
     }
 
     public void addComment(Comment comment){
+
+        User u = userRepository.findById(comment.getUser_id());
+        Post p = postRepository.findById(comment.getPost_id());
+
+        if(u==null){
+            throw new ApiException("user not exist");
+        }
+
+        if(p==null){
+            throw new ApiException("post not exist");
+        }
+
+
         commentRepository.save(comment);
     }
 
     public void updateComment( int id , Comment comment){
+
+        User u = userRepository.findById(comment.getUser_id());
+        Post p = postRepository.findById(comment.getPost_id());
+
+        if(u==null){
+            throw new ApiException("user not exist");
+        }
+
+        if(p==null){
+            throw new ApiException("post not exist");
+        }
+
+
         Comment c = commentRepository.findById(id);
         if(c == null){
             throw new ApiException("comment not exist");
